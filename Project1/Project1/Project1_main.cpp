ea5c3e856c154ee5bfd46470ecb0e9d0da1bc597
Aleks <aleks.the.architect@gmail.com>
2020-04-24 18:42:44 +0300

    Assignment 06

--- Project1/Project1/Project1_main.cpp
+++ Project1/Project1/Project1_main.cpp
@@ -2,6 +2,8 @@
 #include<iostream>
 #include<math.h>
 #include<ctime>
+/*#include <stdio.h>
+#include <stdlib.h> */
 
 
 
@@ -129,20 +131,238 @@ int print_2str(int h, int w, char a)
 } */
 
 //Template
-template < class T1, class T2, class T3, class T4 >
+/*template < class T1, class T2, class T3, class T4 >
 void Temp_Func(T1 a, T2 b, T3 c, T4 d)
 {
 	cout << a << N_LINE << b << N_LINE << c << N_LINE << d << N_LINE;
+}*/
+
+
+//I've provided "min" and "max" functions in                                                                                      
+//case they are useful to you                                                                                                     
+int min(int a, int b) {
+	if (a < b) {
+		return a;
+	}
+	return b;
+}
+int max(int a, int b) {
+	if (a > b) {
+		return a;
+	}
+	return b;
+}
+
+//Declare your rectangle structure here!                                                                                          
+struct rect {
+	int x;
+	int y;
+	int width;
+	int height;
+};
+typedef struct rect rectangle;
+
+rectangle canonicalize(rectangle r) {
+	//WRITE THIS FUNCTION
+		if (r.width < 0)
+		{
+			r.width = r.width + (r.width * -2);
+			r.x = r.x - r.width;
+		};
+		if (r.height < 0)
+		{
+			r.height = r.height + (r.height * -2);
+			r.y = r.y - r.height;
+		};
+	return r;
 }
 
 
-int  main ()
+rectangle intersection(rectangle r1, rectangle r2) {
+	//WRITE THIS FUNCTION   
+	r1 = canonicalize(r1);
+	r2 = canonicalize(r2);
+	int x1_2 = r1.x + r1.width;
+	int x2_2 = r2.x + r2.width;
+	int y1_2 = r1.y + r1.height;
+	int y2_2 = r2.y + r2.height;
+
+	if ((r2.x >= r1.x  &&  x2_2 <= x1_2) && (r2.y >= r1.y  &&  y2_2 <= y1_2))
+	{
+		return r2;
+	}
+	if ((r2.x <= r1.x  &&  x2_2 >= x1_2) && (r2.y <= r1.y  &&  y2_2 >= y1_2))
+	{
+		return r1;
+	}
+	else {
+		if (
+			((r1.x >= r2.x  &&  r1.x <= x2_2) || (x1_2 >= r2.x  &&  x1_2 <= x2_2)) &&
+			((r1.y >= r2.y  &&  r1.y <= y2_2) || (y1_2 >= r2.y  &&  y1_2 <= y2_2))
+			)
+		{
+
+			if (r1.x <= r2.x)
+			{
+				r1.x = r2.x;
+				r1.width = x1_2 - r2.x;
+			}
+			else {
+				r1.width = x2_2 - r1.x;
+			}
+			if (r1.y <= r2.y)
+			{
+				r1.y = r2.y;
+				r1.height = y1_2 - r2.y;
+			}
+			else {
+				r1.height = y2_2 - r1.y;
+			}
+		}
+		else {
+			r1.width = 0;
+			r1.height = 0;
+		}
+	}
+	return r1;
+}
+
+
+//You should not need to modify any code below this line                                                                          
+void printRectangle(rectangle r) {
+	r = canonicalize(r);
+	if (r.width == 0 && r.height == 0) {
+		printf("<empty>\n");
+	}
+	else {
+		printf("(%d,%d) to (%d,%d)\n", r.x, r.y,
+			r.x + r.width, r.y + r.height);
+	}
+}
+
+int  main (void)
 {
 	setlocale(LC_ALL, "RUS");
 	
-	//Template
-	Temp_Func(true, 0x2F, 3.849, "WOW!!!");
+		rectangle r1;
+		rectangle r2;
+		rectangle r3;
+		rectangle r4; 
 
+		r1.x = 2;
+		r1.y = 3;
+		r1.width = 5;
+		r1.height = 6;
+		printf("r1 is ");
+		printRectangle(r1);
+
+		r2.x = 4;
+		r2.y = 5;
+		r2.width = -5;
+		r2.height = -7;
+		printf("r2 is ");
+		printRectangle(r2);
+
+		r3.x = -2;
+		r3.y = 7;
+		r3.width = 7;
+		r3.height = -10;
+		printf("r3 is ");
+		printRectangle(r3);
+
+		r4.x = 0;
+		r4.y = 7;
+		r4.width = -4;
+		r4.height = 2;
+		printf("r4 is ");
+		printRectangle(r4); 
+
+		//test everything with r1                                                                                                       
+		rectangle i = intersection(r1, r1);
+		printf("intersection(r1,r1): ");
+		printRectangle(i);
+
+		i = intersection(r1, r2);
+		printf("intersection(r1,r2): ");
+		printRectangle(i);
+
+		i = intersection(r1, r3);
+		printf("intersection(r1,r3): ");
+		printRectangle(i);
+
+		i = intersection(r1, r4);
+		printf("intersection(r1,r4): ");
+		printRectangle(i);
+
+		//test everything with r2                                                                                                       
+		i = intersection(r2, r1);
+		printf("intersection(r2,r1): ");
+		printRectangle(i);
+
+		i = intersection(r2, r2);
+		printf("intersection(r2,r2): ");
+		printRectangle(i);
+
+		i = intersection(r2, r3);
+		printf("intersection(r2,r3): ");
+		printRectangle(i);
+
+		i = intersection(r2, r4);
+		printf("intersection(r2,r4): ");
+		printRectangle(i);
+
+		//test everything with r3                                                                                                       
+		i = intersection(r3, r1);
+		printf("intersection(r3,r1): ");
+		printRectangle(i);
+
+		i = intersection(r3, r2);
+		printf("intersection(r3,r2): ");
+		printRectangle(i);
+
+		i = intersection(r3, r3);
+		printf("intersection(r3,r3): ");
+		printRectangle(i);
+
+		i = intersection(r3, r4);
+		printf("intersection(r3,r4): ");
+		printRectangle(i);
+
+		//test everything with r4                                                                                                       
+		i = intersection(r4, r1);
+		printf("intersection(r4,r1): ");
+		printRectangle(i);
+
+		i = intersection(r4, r2);
+		printf("intersection(r4,r2): ");
+		printRectangle(i);
+
+		i = intersection(r4, r3);
+		printf("intersection(r4,r3): ");
+		printRectangle(i);
+
+		i = intersection(r4, r4);
+		printf("intersection(r4,r4): ");
+		printRectangle(i); 
+
+		/*cout << r1.x << "  " << r1.y << "  " << r1.width << "  " << r1.height << endl;
+		cout << r2.x << "  " << r2.y << "  " << r2.width << "  " << r2.height << endl;
+		cout << r3.x << "  " << r3.y << "  " << r3.width << "  " << r3.height << endl;
+		cout << r4.x << "  " << r4.y << "  " << r4.width << "  " << r4.height << endl;*/
+
+		return EXIT_SUCCESS;
+
+
+
+
+
+
+
+
+
+
+	//Template
+	/*Temp_Func(true, 0x2F, 3.849, "WOW!!!");*/
 
 	//Square
 	/*int width;
