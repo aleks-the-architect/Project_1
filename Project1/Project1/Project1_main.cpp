4a42487128108ddefb0928744754ea96727531dd
Aleks <aleks.the.architect@gmail.com>
2020-05-13 14:25:15 +0300

    Update 13/05/2020

--- Project1/Project1/Project1_main.cpp
+++ Project1/Project1/Project1_main.cpp
@@ -4,6 +4,7 @@
 #include<ctime>
 #include <stdio.h>
 #include <stdlib.h> 
+#include "Header.h"
 
 
 
@@ -250,20 +251,442 @@ void retirement(int startAge, double initial, retire_info working, retire_info r
 	}
 } */
 
-
-
-
-
-
-  int main(void)
+//Recuirse
+/*int recuirse(int a)
 {
-	setlocale(LC_ALL, "RUS");
+	if (a == 0)
+	{
+		return 0;
+	}
+ if (a == 1)
+{
+	return 1;
+}
+ return a * recuirse(a - 1);
+} */
+
+// CARDS Course 2
+/*void assert_card_valid(card_t c) 
+{
+	if (c.value >= 2 && c.value <= VALUE_ACE); 
+	_ASSERT(c.value != 0); //c.value != 0
+	
+	if (c.suit >= SPADES && c.suit <= CLUBS);
+	_ASSERT(c.suit != NUM_SUITS);
+}
+
+const char * ranking_to_string(hand_ranking_t r) {
+	switch (r)
+	{
+	case STRAIGHT_FLUSH: return "STRAIGHT FLUSH"; break;
+	case FOUR_OF_A_KIND: return "FOUR OF A KIND"; break;
+	case FULL_HOUSE: return "FULL HOUSE"; break;
+	case FLUSH: return "FLUSH"; break;
+	case STRAIGHT: return "STRAIGHT"; break;
+	case THREE_OF_A_KIND: return "THREE OF A KIND"; break;
+	case TWO_PAIR: return "TWO PAIR"; break;
+	case PAIR: return "PAIR"; break;
+	case NOTHING: return "NOTHING"; break;
+	}
+	return 0;
+}
+
+char value_letter(card_t c) 
+{
+	if (c.value == 10)
+	{
+		return 48;
+	}
+	if (c.value >= 2  &&  c.value <= 9)
+	{
+		return 48 + c.value;
+	}
+	if (c.value == VALUE_ACE)
+	{
+		return 'A';
+	}
+	if (c.value == VALUE_KING)
+	{
+		return 'K';
+	}
+	if (c.value == VALUE_QUEEN)
+	{
+		return 'Q';
+	}
+	if (c.value == VALUE_JACK)
+	{
+		return 'J';
+	}
+	return 0;
+}
+
+
+char suit_letter(card_t c) {
+	switch (c.suit)
+	{
+	case SPADES: return 's'; break;
+	case HEARTS: return 'h'; break;
+	case DIAMONDS: return 'd'; break;
+	case CLUBS: return 'c'; break;
+	case NUM_SUITS: return '?';
+	} 
+	return 0;
+}
+
+ void print_card(card_t c) 
+ {
+	 printf("%c%c" , value_letter(c), suit_letter(c));
+}
+
+card_t card_from_letters(char value_let, char suit_let) 
+{
+	card_t temp;
+	switch (value_let)
+	{
+	case '0': temp.value = 10; break;
+	case '1': temp.value = 1; break;
+	case '2': temp.value = 2; break;
+	case '3': temp.value = 3; break;
+	case '4': temp.value = 4; break;
+	case '5': temp.value = 5; break;
+	case '6': temp.value = 6; break;
+	case '7': temp.value = 7; break;
+	case '8': temp.value = 8; break;
+	case '9': temp.value = 9; break;
+	case 'A': temp.value = VALUE_ACE; break;
+	case 'K': temp.value = VALUE_KING; break;
+	case 'Q': temp.value = VALUE_QUEEN; break;
+	case 'J': temp.value = VALUE_JACK; break;
+	default:  temp.value = 0;
+	}
+
+	switch  (suit_let)
+	{
+	case 's': temp.suit = SPADES; break; //s
+	case 'h': temp.suit = HEARTS; break; //h
+	case 'd': temp.suit = DIAMONDS; break; //d
+	case 'c': temp.suit = CLUBS; break; //c
+	default:  temp.suit = NUM_SUITS;
+	};
+
+	assert_card_valid (temp);
+	return temp;
+}
+
+card_t card_from_num(unsigned c) 
+{
+	card_t temp;
+	int num_suit = c / 13;
+	int num_value = c % 13;
+
+	if (num_suit <= 3)
+	{
+		if (num_value >= 0 && num_value <= 8)
+		{
+			temp.value = num_value + 2;
+		}
+		if (num_value == 9)
+		{
+			temp.value = VALUE_JACK;
+		}
+		if (num_value == 10)
+		{
+			temp.value = VALUE_QUEEN;
+		}
+		if (num_value == 11)
+		{
+			temp.value = VALUE_KING;
+		}
+		if (num_value == 12)
+		{
+			temp.value = VALUE_ACE;
+		}
+
+		switch (num_suit)
+		{
+		case 0: temp.suit = SPADES; break;
+		case 1: temp.suit = HEARTS; break;
+		case 2: temp.suit = DIAMONDS; break;
+		case 3: temp.suit = CLUBS; break;
+		}
+	}
+	else
+	{
+		temp.value = num_value + 2;
+		temp.suit = NUM_SUITS;
+	}
+	assert_card_valid(temp);
+	return temp;
+}  */
+
+//Assignment 11 Course 3
+/*void g(int x, int * y) {
+	printf("In g, x = %d, *y = %d\n", x, *y);
+	x++;
+	*y = *y - x;
+	y = &x;
+}
+
+void f(int * a, int b) {
+	printf("In f, *a = %d, b = %d\n", *a, b);
+	*a += b;
+	b *= 2;
+	g(*a, &b);
+	printf("Back in f, *a = %d, b = %d\n", *a, b);
+} */
+
+//Assignment 12 Course 3
+/*int f(int ** r, int ** s) {
+	int temp = ** r;
+	int temp2 = **s;
+	int * z = *r;
+	*r = *s;
+	*s = z;
+	printf("**r = %d\n", **r);
+	printf("**s = %d\n", **s);
+	*z += 3;
+	**s -= 8;
+	**r -= 19;
+	return temp + temp2;
+} */
+
+//SWAP
+  /*template <typename T1, typename T2> 
+void swap(T1 &pa, T2 &pb)
+{
+	int temp = pa;
+	pa = pb;
+	pb = temp;
+
+} */
+
+//Array
+/*int SumArray(int *arr, int n)
+{
+	int answer = 0;
+	for (int i = 0; i < n; i++)
+	{
+		answer = answer + arr[i];
+	}
+	return answer;
+} */
+
+//Array MAX
+  /*int * arrayMax(int * array, int n) 
+{
+	if (n == 0)
+	{
+		return NULL;
+	}
+
+	int *largest = array;
+	for (int i = 1; i < n; i++)
+	{
+		if ( *(array + i) > *largest)
+		{
+			*largest = *(array + i);
+		}
+	}
+	return largest;
+}*/
+
+//Assignment 13 Course 3
+  /*void doTest(int * array, int n) {
+	printf("arrayMax(");
+	if (array == NULL) {
+		printf("NULL");
+	}
+	else {
+		printf("{");
+		for (int i = 0; i < n; i++) {
+			printf("%d", array[i]);
+			if (i < n - 1) {
+				printf(", ");
+			}
+		}
+		printf("}");
+	}
+	printf(", %d) is \n", n);
+	int * p = arrayMax(array, n);
+	if (p == NULL) {
+		printf("NULL\n");
+	}
+	else {
+		printf("%d\n", *p);
+	}
+}*/
+
+
+size_t maxSeq(int * arrayLenght, size_t n)
+{
+	size_t maxLenSeq = *arrayLenght;
+	int *arr_1 = arrayLenght;
+	//int sizeArrLen = sizeof(arrayLenght) / sizeof(arrayLenght[0]);
+	for (int i = 1; i < n; i++)
+	{
+		if ( *(arr_1 + i) > *arr_1)
+		//if (arrayLenght[i + 1] > arrayLenght[i])
+		{
+			maxLenSeq = *(arr_1 + i);
+			//maxLenSeq = arrayLenght[i + 1];
+		}
+	}
+	return maxLenSeq;
+}
+
 	
 
 
 
 
+int main(void)
+{
+	setlocale(LC_ALL, "RUS");
 
+
+		int array[] = { 1000,500,100,55,22,11,1,0 };
+		
+		int *pArr = array;
+		size_t maxLenSeq = 0;
+	    size_t curLenSeq = 0;
+		int sizeArray = sizeof(array) / sizeof(array[0]);
+	
+		if (sizeArray == 0)
+		{
+			return maxLenSeq = 0;
+		}
+	
+			for (size_t i = 0; i < sizeArray; i++)
+			{
+					for ( size_t j = 0;  pArr[ i + 1] > pArr[i] &&  i < sizeArray;  j++ )
+					{
+						curLenSeq = j + 2;
+						i++;
+					}
+	
+			
+				if (curLenSeq > maxLenSeq)
+				{
+					maxLenSeq = curLenSeq;
+				}
+				if (maxLenSeq == 0)
+				{
+					maxLenSeq = 1;
+				}
+	
+				/*if (arrayLenght[0] > maxLenSeq)
+				{
+					maxLenSeq = arrayLenght[0];
+				}
+				if (arrayLenght[0] == 0)
+				{
+					maxLenSeq = 1;
+				} */
+	
+			}
+			
+		/*for (int s = 0; s < 2; s++)
+		{
+			cout << arrayLenght[s] << " ";
+		}*/
+	
+		cout << endl << "Max lenght in number:\t" << maxLenSeq << endl;
+		cout << "Size of array:\t" << sizeArray << endl;
+		
+		return maxLenSeq;
+		 //return EXIT_SUCCESS;
+	}
+
+
+	/*int arr[500]{};
+	for (int i = 0; i < 500; i++)
+	{
+		arr[i] = i;
+		cout << i <<", ";
+	}
+}*/
+
+
+//Assignment 13 Course 3
+	/*doTest(array1, 8);
+	doTest(array2, 7);
+	doTest(array3, 6);
+	doTest(NULL, 0);
+	doTest(array1, 0); */
+
+	//return EXIT_SUCCESS;
+
+	//Array
+	/*int sum;
+	const int SIZE = 4;
+
+	int arr[SIZE]{ 3, 5, 4, 103 };
+	cout << SumArray (arr , SIZE) << endl;
+	cout << arr[5];*/
+
+	//SWAP
+  /*int a = 4, b = 7;
+cout << "Before a = " << a << endl;
+cout << "Before b = " << b << endl;
+
+swap (a, b);
+cout << "After a = " << a << endl;
+cout << "After b = " << b << endl;
+cout << "a - b = " << b - a << endl;*/
+
+	//Assignment 13 Course 3
+	/*int anArray[] = { 5,16,33,99 };
+	int * p = anArray;
+	printf("*p = %d\n", *p);
+	p++;
+	printf("Now *p = %d\n", *p);
+	int * q = &anArray[3];
+	int ** x = &q;
+	**x = 12;
+	*x = p;
+	**x = 42;
+	q[1] = 9;
+	for (int i = 0; i < 4; i++) {
+		printf("anArray[%d] = %d\n", i, anArray[i]);
+	}
+	return EXIT_SUCCESS;
+  } */
+
+	//Assignment 12 Course 3
+	/*int a = 80;
+	int b = 12;
+	int * p = &a;
+	int * q = &b;
+	int x = f(&p, &q);
+	printf("x = %d\n", x);
+	printf("*p = %d\n", *p);
+	printf("*q = %d\n", *q);
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	return EXIT_SUCCESS; */
+
+	//Assignment 11 Course 3
+	/*int x = 3;
+	int y = 4;
+	f(&x, y);
+	printf("In main: x = %d, y = %d\n", x, y);
+	return EXIT_SUCCESS; */
+
+	// CARDS Course 2
+	/*for (int i = 0; i < 52; i++)
+	{
+	  print_card(card_from_num(i));
+	}
+	char val = 'A';
+	char suit = 's';
+	card_t a = card_from_letters(val, suit);
+	print_card(a);
+
+	cout << value_letter(a) << suit_letter(a);
+	print_card(a); */
+
+	//Recuirse
+	/*cout << recuirse(9) << endl; */
 
 	//RETIRE CULCULATOR
 	/*int startAge = 327;
@@ -535,4 +958,4 @@ cin >> v3;
 
 
 	 /*_ASSERT(hor_vert == 1); */
-	}
+
